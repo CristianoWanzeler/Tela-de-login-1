5714af35110a36031ee346aca1fb9c77b5892cdc
CristianoWanzeler <cr_w3@outlook.com>
2025-07-26 11:54:11 -0300

    Exlusão de classe

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Cadastro from "./Cadastro";
-import Cafeteria from "./Cafeteria";
 
 function App(){
   return(
@@ -10,7 +9,6 @@ function App(){
         <Route path="/" element={<Login/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/cadastro" element={<Cadastro/>} />
-        <Route path="/cafeteria" element={<Cafeteria/>} />
       </Routes>
     </Router>
   )
--- src/Cafeteria.jsx
+++
@@ -1,9 +0,0 @@
-function Cafeteria (){
-    return(
-        <header className="header">
-            <a href="#"><img src="img/logo 1.png" alt="" /></a>
-        </header>
-        )
-    )
-}
-export default Cafeteria;
